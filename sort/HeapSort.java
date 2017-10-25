1ee7ef8ff16a380f3bf9265ddb82f7ad6da7b4d0
chencong <337812462@qq.com>
2017-10-25 21:34:27 +0800

    堆排序

---
+++ sort/HeapSort.java
@@ -0,0 +1,65 @@
+package sort;
+/*
+ * 功能：堆排序(不稳定的排序算法)【大顶堆】
+ * 
+ * 原理：
+ * 
+ * 时间复杂度：平均O(nlogn),最好O(nlogn),最差O(nlogn)
+ * 
+ * 空间复杂度：O(1)
+ * */
+public class HeapSort {
+	public static void main(String[] args){
+		int[]  arr = {5, 3, 8, 6, 4, -1, 0, 15, 7, 23, 9, 6, 6, 7, 8, 9};
+		heapSort(arr);
+		for(int i = 0; i < arr.length; i++){
+			System.out.println(arr[i]);
+		}
+	}
+	
+	public static void heapSort(int[] arr){
+		if(arr == null || arr.length == 0){
+			return;
+		}
+		//建立大顶堆
+		for(int i = arr.length/2; i >= 0; i--){
+			heapAdjust(arr, i, arr.length - 1);
+		}
+		for(int i = arr.length - 1; i >=0; i--){
+			swap(arr, 0, i);
+			heapAdjust(arr, 0, i - 1);
+		}
+	}
+	
+/*
+ * 堆筛选，除了start之外，start~end均满足大顶堆的定义
+ * 调整之后start~end称为有一个大顶堆
+ * @param arr 待调整数组
+ * @param start 起始指针
+ * @param end 结束指针
+ * */
+	public static void heapAdjust(int[] arr, int start, int end){
+		int temp = arr[start];
+		for(int i = 2 * start + 1; i <= end; i*=2){
+			//左右孩子的节点分别为2*i+1，2*i+2
+			
+			//选择出左右孩子较小的下标
+			if(i < end && arr[i] < arr[i + 1]){
+				i++;
+			}
+			if(temp >= arr[i]){
+				break; //已经为大顶堆，=保持稳定性
+			}
+			arr[start] = arr[i]; //将子节点上移
+			start = i; //下一轮筛选 
+		}
+		arr[start] = temp; // 插入正确的位置
+	}
+	
+	public static void swap(int[] arr, int i, int j){
+		int temp;
+		temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] =temp;
+	}
+}
